186b5646d95f6ae26e9303eb9071b77a08ec5c3d
moooooogen <66089268+moooooogen@users.noreply.github.com>
2020-12-24 22:16:47 -0600

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,24 @@
+// --== CS400 File Header Information ==--
+// Name: Megan Le
+// Email: mle25@wisc.edu
+// Team: AF
+// Role: Back End Developer 
+// TA: Sophie Stephenson
+// Lecturer: Gary Dahl
+// Notes to Grader: <optional extra notes>
+import java.util.NoSuchElementException;
+
+public interface Table<K, V> {
+
+		public boolean put(K key, V value);
+
+		public V get(K key) throws NoSuchElementException;
+
+		public int size();
+
+		public boolean containsKey(K key);
+
+		public String[] remove(K key);
+
+		public void clear();
+}
