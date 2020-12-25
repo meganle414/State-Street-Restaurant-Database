20f1c546ae69c372bcfd349c855bc2c42082d35d
moooooogen <66089268+moooooogen@users.noreply.github.com>
2020-12-24 22:14:39 -0600

    Update List.java

--- List.java
+++ List.java
@@ -7,7 +7,7 @@
 // Lecturer: Gary Dahl
 // Notes to Grader: <optional extra notes>
 public interface List {
-	public void addRestaurant(String address, String[] info);
+  public void addRestaurant(String address, String[] info);
   public String[] removeRestaurant(String address);
   public String[] searchRestaurant(String address);
   public void clearTable();
